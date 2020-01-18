c51e4fef01d310140b502b0440bb20c529f88536
Manuel <mzama@gmail.com>
2020-01-18 16:24:16 -0500

    Actualizado

---
+++ CategoriaService.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.springbootapp.services;
+
+import java.util.List;
+
+import pe.edu.tecsup.springbootapp.models.Categoria;
+
+public interface CategoriaService {
+
+	public List<Categoria> listar();
+	
+}
